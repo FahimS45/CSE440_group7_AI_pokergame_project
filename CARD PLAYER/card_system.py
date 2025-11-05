6f115769e294575ecbcbf95220cd343df84b5283
Fahim Shahriar <128742108+FahimS45@users.noreply.github.com>
2025-11-05 18:24:51 +0600

    Delete CARD PLAYER directory

--- CARD PLAYER/card_system.py
+++
@@ -1,9 +0,0 @@
-class Deck:
-    def __init__(self):
-        self.cards = [("Hearts", "A"), ("Spades", "K")]
-
-    def shuffle(self):
-        pass
-
-    def deal(self, num_cards):
-        return self.cards[:num_cards]
