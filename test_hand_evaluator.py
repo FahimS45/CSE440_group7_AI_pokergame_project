f77e1a7cfaab0559c46b1e77e518359ff6470052
Sumaya Akter <sumaya.akter01@northsouth.edu>
2025-11-05 22:06:18 +0600

    Have some changes based on our abstract file.

--- test_hand_evaluator.py
+++ test_hand_evaluator.py
@@ -1,89 +1,102 @@
-"""
-Unit tests for Hand Evaluation System
-"""
-
 import unittest
-from hand_evaluator import HandEvaluator, HandRank
+from hand_evaluator import PokerHandEvaluator
 
-class TestHandEvaluator(unittest.TestCase):
+class TestPokerHandEvaluator(unittest.TestCase):
+    
     def setUp(self):
-        self.evaluator = HandEvaluator()
+        self.evaluator = PokerHandEvaluator()
     
     def test_royal_flush(self):
-        hand = [('10', 'H'), ('J', 'H'), ('Q', 'H'), ('K', 'H'), ('A', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.ROYAL_FLUSH)
+        hand = ['10S', 'JS', 'QS', 'KS', 'AS']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 9)  # Straight flush
+        self.assertEqual(tiebreakers, [14])
     
     def test_straight_flush(self):
-        hand = [('9', 'H'), ('10', 'H'), ('J', 'H'), ('Q', 'H'), ('K', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.STRAIGHT_FLUSH)
+        hand = ['9H', '10H', 'JH', 'QH', 'KH']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 9)
+        self.assertEqual(tiebreakers, [13])
     
     def test_four_of_a_kind(self):
-        hand = [('A', 'H'), ('A', 'D'), ('A', 'C'), ('A', 'S'), ('K', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.FOUR_OF_A_KIND)
+        hand = ['5S', '5H', '5D', '5C', 'AS']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 8)
+        self.assertEqual(tiebreakers, [5, 14])
     
     def test_full_house(self):
-        hand = [('A', 'H'), ('A', 'D'), ('A', 'C'), ('K', 'S'), ('K', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.FULL_HOUSE)
+        hand = ['8S', '8H', '8D', 'KC', 'KH']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 7)
+        self.assertEqual(tiebreakers, [8, 13])
     
     def test_flush(self):
-        hand = [('2', 'H'), ('5', 'H'), ('7', 'H'), ('9', 'H'), ('K', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.FLUSH)
+        hand = ['2D', '5D', '7D', '9D', 'QD']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 6)
+        self.assertEqual(tiebreakers, [12, 9, 7, 5, 2])
     
     def test_straight(self):
-        hand = [('9', 'H'), ('10', 'D'), ('J', 'C'), ('Q', 'S'), ('K', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.STRAIGHT)
+        hand = ['6C', '7H', '8S', '9D', '10C']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 5)
+        self.assertEqual(tiebreakers, [10])
     
     def test_wheel_straight(self):
-        hand = [('A', 'H'), ('2', 'D'), ('3', 'C'), ('4', 'S'), ('5', 'H')]
-        rank, tie_breakers = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.STRAIGHT)
-        self.assertEqual(tie_breakers[0], 5)  # High card should be 5, not Ace
+        hand = ['AS', '2H', '3D', '4C', '5S']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 5)
+        self.assertEqual(tiebreakers, [5])
     
     def test_three_of_a_kind(self):
-        hand = [('A', 'H'), ('A', 'D'), ('A', 'C'), ('K', 'S'), ('Q', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.THREE_OF_A_KIND)
+        hand = ['JS', 'JH', 'JD', '4C', '7S']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 4)
+        self.assertEqual(tiebreakers, [11, 7, 4])
     
     def test_two_pair(self):
-        hand = [('A', 'H'), ('A', 'D'), ('K', 'C'), ('K', 'S'), ('Q', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.TWO_PAIR)
+        hand = ['9S', '9H', '4D', '4C', 'AS']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 3)
+        self.assertEqual(tiebreakers, [9, 4, 14])
     
     def test_one_pair(self):
-        hand = [('A', 'H'), ('A', 'D'), ('K', 'C'), ('Q', 'S'), ('J', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.ONE_PAIR)
+        hand = ['QS', 'QH', '8D', '3C', '2S']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 2)
+        self.assertEqual(tiebreakers, [12, 8, 3, 2])
     
     def test_high_card(self):
-        hand = [('A', 'H'), ('K', 'D'), ('Q', 'C'), ('J', 'S'), ('9', 'H')]
-        rank, _ = self.evaluator.evaluate_hand(hand)
-        self.assertEqual(rank, HandRank.HIGH_CARD)
+        hand = ['AS', 'KD', 'QC', 'JH', '9S']
+        rank, tiebreakers = self.evaluator.evaluate_hand(hand)
+        self.assertEqual(rank, 1)
+        self.assertEqual(tiebreakers, [14, 13, 12, 11, 9])
     
     def test_hand_comparison(self):
-        # Royal flush vs Straight flush
-        royal_flush = [('10', 'H'), ('J', 'H'), ('Q', 'H'), ('K', 'H'), ('A', 'H')]
-        straight_flush = [('9', 'H'), ('10', 'H'), ('J', 'H'), ('Q', 'H'), ('K', 'H')]
-        self.assertEqual(self.evaluator.compare_hands(royal_flush, straight_flush), 1)
+        # Test hand1 wins (flush vs straight)
+        hand1 = ['2D', '5D', '7D', '9D', 'QD']  # Flush
+        hand2 = ['6C', '7H', '8S', '9D', '10C']  # Straight
+        result = self.evaluator.compare_hands(hand1, hand2)
+        self.assertEqual(result, 1)
         
-        # Same hand type, different high cards
-        flush1 = [('A', 'H'), ('K', 'H'), ('Q', 'H'), ('J', 'H'), ('9', 'H')]
-        flush2 = [('K', 'D'), ('Q', 'D'), ('J', 'D'), ('10', 'D'), ('8', 'D')]
-        self.assertEqual(self.evaluator.compare_hands(flush1, flush2), 1)
+        # Test hand2 wins (full house vs two pair)
+        hand1 = ['9S', '9H', '4D', '4C', 'AS']  # Two pair
+        hand2 = ['8S', '8H', '8D', 'KC', 'KH']  # Full house
+        result = self.evaluator.compare_hands(hand1, hand2)
+        self.assertEqual(result, -1)
         
-        # Tie
-        same_hand1 = [('A', 'H'), ('K', 'D'), ('Q', 'C'), ('J', 'S'), ('10', 'H')]
-        same_hand2 = [('A', 'D'), ('K', 'H'), ('Q', 'S'), ('J', 'C'), ('10', 'D')]
-        self.assertEqual(self.evaluator.compare_hands(same_hand1, same_hand2), 0)
+        # Test tie (same high cards)
+        hand1 = ['AS', 'KD', 'QC', 'JH', '9S']
+        hand2 = ['AH', 'KS', 'QD', 'JC', '9H']
+        result = self.evaluator.compare_hands(hand1, hand2)
+        self.assertEqual(result, 0)
     
-    def test_hand_rank_string(self):
-        self.assertEqual(self.evaluator.hand_rank_to_string(HandRank.ROYAL_FLUSH), "Royal Flush")
-        self.assertEqual(self.evaluator.hand_rank_to_string(HandRank.ONE_PAIR), "One Pair")
+    def test_same_rank_different_kickers(self):
+        # Both have pair of 10s, but different kickers
+        hand1 = ['10S', '10H', 'AS', 'KD', '3C']  # A, K kickers
+        hand2 = ['10D', '10C', 'AS', 'QD', '4C']  # A, Q kickers
+        result = self.evaluator.compare_hands(hand1, hand2)
+        self.assertEqual(result, 1)  # hand1 wins with better kicker
 
 if __name__ == '__main__':
     unittest.main()
